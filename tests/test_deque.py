61d6b004478f2c90b67c932354ce4067c4df9537
omtarful <omtarful@gmail.com>
2023-01-11 21:58:11 +0200

    Converts unit tests in deque to pytest

--- tests/test_deque.py
+++ tests/test_deque.py
@@ -1,12 +1,10 @@
 """Unit tests for the Deque class."""
 
-import unittest
-
 from lib.deque import Deque
+import pytest
 
-
-class TestDeque(unittest.TestCase):
-    def setUp(self):
+class TestDeque:
+    def setup_method(self):
         # Create queues for the tests to use
         self.new = Deque()
         # Test combination of add/remove
@@ -27,55 +25,55 @@ class TestDeque(unittest.TestCase):
         self.from_rear.add_rear(5)
 
     def test_length(self):
-        self.assertEqual(len(self.new), 0)
-        self.assertEqual(len(self.empty), 0)
-        self.assertEqual(len(self.from_front), 4)
+        assert (len(self.new) == 0)
+        assert (len(self.empty) == 0)
+        assert (len(self.from_front) == 4)
 
     def test_is_empty(self):
-        self.assertTrue(self.new.is_empty())
-        self.assertTrue(self.empty.is_empty())
-        self.assertFalse(self.from_front.is_empty())
+        assert (self.new.is_empty())
+        assert (self.empty.is_empty())
+        assert not (self.from_front.is_empty())
 
     def test_str(self):
-        self.assertEqual(str(self.new), str(self.empty))
-        self.assertEqual(str(self.from_front), str(self.from_rear))
+        assert (str(self.new) == str(self.empty))
+        assert (str(self.from_front) == str(self.from_rear))
 
     def test_fifo_order(self):
-        self.assertEqual(self.from_front.remove_rear(), 5)
-        self.assertEqual(self.from_front.remove_rear(), 2)
-        self.assertEqual(self.from_front.remove_rear(), 7)
-        self.assertEqual(self.from_front.remove_rear(), 2)
+        assert (self.from_front.remove_rear() == 5)
+        assert (self.from_front.remove_rear() == 2)
+        assert (self.from_front.remove_rear() == 7)
+        assert (self.from_front.remove_rear() == 2)
         # FIFO is opposite order in other deque
-        self.assertEqual(self.from_rear.remove_front(), 2)
-        self.assertEqual(self.from_rear.remove_front(), 7)
-        self.assertEqual(self.from_rear.remove_front(), 2)
-        self.assertEqual(self.from_rear.remove_front(), 5)
+        assert (self.from_rear.remove_front() == 2)
+        assert (self.from_rear.remove_front() == 7)
+        assert (self.from_rear.remove_front() == 2)
+        assert (self.from_rear.remove_front() == 5)
 
     def test_lifo_order(self):
-        self.assertEqual(self.from_front.remove_front(), 2)
-        self.assertEqual(self.from_front.remove_front(), 7)
-        self.assertEqual(self.from_front.remove_front(), 2)
-        self.assertEqual(self.from_front.remove_front(), 5)
+        assert (self.from_front.remove_front() == 2)
+        assert (self.from_front.remove_front() == 7)
+        assert (self.from_front.remove_front() == 2)
+        assert (self.from_front.remove_front() == 5)
         # LIFO is opposite order in other deque
-        self.assertEqual(self.from_rear.remove_rear(), 5)
-        self.assertEqual(self.from_rear.remove_rear(), 2)
-        self.assertEqual(self.from_rear.remove_rear(), 7)
-        self.assertEqual(self.from_rear.remove_rear(), 2)
+        assert (self.from_rear.remove_rear() == 5)
+        assert (self.from_rear.remove_rear() == 2)
+        assert (self.from_rear.remove_rear() == 7)
+        assert (self.from_rear.remove_rear() == 2)
 
     def test_access_to_empty(self):
-        with self.assertRaises(AssertionError):
+        with pytest.raises(AssertionError):
             self.new.front()
-        with self.assertRaises(AssertionError):
+        with pytest.raises(AssertionError):
             self.empty.rear()
-        with self.assertRaises(AssertionError):
+        with pytest.raises(AssertionError):
             self.new.remove_rear()
-        with self.assertRaises(AssertionError):
+        with pytest.raises(AssertionError):
             self.empty.remove_front()
 
     def test_membership(self):
-        self.assertFalse(2 in self.new)
-        self.assertFalse("hi" in self.empty)
-        self.assertTrue(2 in self.from_front)
-        self.assertTrue(5 in self.from_front)
-        self.assertTrue(7 in self.from_front)
-        self.assertFalse(3 in self.from_front)
+        assert not (2 in self.new)
+        assert not ("hi" in self.empty)
+        assert (2 in self.from_front)
+        assert (5 in self.from_front)
+        assert (7 in self.from_front)
+        assert not (3 in self.from_front)
